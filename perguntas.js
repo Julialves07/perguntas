13d61d21d07b616683a4dc3bfadf8fc08ed8f767
Julialves07 <ja4981412@gmail.com>
2024-11-29 09:16:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Explique oque é programação orientada a objetos?',
+    'é um paradigma de programação que usa objetos em vez de procedimentos e funções'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha',
+    'A capital da espanha é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'defina classe na programação orientada a objetos?',
+    'uma classe é composta de objetos que tem varios métodos comuns'
+)
+
+criaCartao(
+    'lingua inglesa',
+    'como se diz what em inglês?',
+    'what em ingles é o que ou qual'
+)
